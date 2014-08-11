98986a1223509447d4f144edbb37dd64e962a886
xsimov <xsimov@gmail.com>
2014-08-11 12:21:21 +0200

    View tasks in the project index implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.create name: "Getting a sim card for Jane"
+Project.create name: "Getting rid of a cold"
+
+Task.create name: "Evaluate prices", project_id: 1, status: "doing"
+Task.create name: "Go and buy it", project_id: 1, status: "todo"
+Task.create name: "Get some legal meds", project_id: 2, status: "todo"
+Task.create name: "Buy tissue", project_id: 2, status: "todo"
+Task.create name: "Stop sneezing", project_id: 2, status: "done"
