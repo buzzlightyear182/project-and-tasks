778ab84fac25fe97985736d9fc9a0b7122bcf65c
xsimov <xsimov@gmail.com>
2014-08-11 11:16:57 +0200

    Implemented index, new and create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,5 +4,18 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.create project_params
+    redirect_to action: 'index'
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name)
+  end
 
 end
